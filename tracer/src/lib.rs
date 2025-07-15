33aa24c16fe0ff878088b26c576eadf3d2e304d2
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2025-07-15 16:50:06 +0200

    use `zucchero`

--- tracer/src/lib.rs
+++ tracer/src/lib.rs
@@ -1,6 +1,4 @@
-use std::{io::Write, sync::Mutex};
-
-use once_cell::sync::OnceCell;
+use std::io::Write;
 
 #[derive(Debug, Default)]
 pub struct State {
@@ -27,40 +25,49 @@ pub struct OpTrace {
     pub val: usize,
 }
 
-static STATE: OnceCell<Mutex<State>> = OnceCell::new();
+mod hidden {
+    use super::State;
+    zucchero::init!(State, expose);
+}
 
 pub fn flush(mut w: impl Write) {
-    let state = STATE.get_or_init(|| Mutex::new(State::default()));
-    let mut guard = state.lock().unwrap();
-    let state = format!("{:#?}", guard);
-    w.write_all(state.as_bytes()).unwrap();
-    *guard = State::default();
-    println!("[tracer] flush");
+    hidden::expose(|state| {
+        let s = format!("{state:#?}");
+        w.write_all(s.as_bytes()).unwrap();
+        *state = State::default();
+        println!("[tracer] flush");
+    });
 }
 
 pub fn on_block(block: &shared::Block) {
-    let state = STATE.get_or_init(|| Mutex::new(State::default()));
-    let mut guard = state.lock().unwrap();
-
-    let trace = BlockTrace { block_id: block.id, txs: Vec::with_capacity(block.txs.len()) };
-    (*guard).traces.push(trace);
+    let trace = BlockTrace {
+        block_id: block.id,
+        txs: Vec::with_capacity(block.txs.len()),
+    };
+    hidden::expose(|state| state.traces.push(trace));
 }
 
 pub fn on_tx(tx: &shared::Tx) {
-    let state = STATE.get_or_init(|| Mutex::new(State::default()));
-    let mut guard = state.lock().unwrap();
-    
-    let block: &mut BlockTrace = guard.traces.last_mut().expect("current block");
-    let trace = TxTrace { block_id: block.block_id, tx_id: tx.id, ops: Vec::with_capacity(tx.ops.len()) };
-    block.txs.push(trace);
+    hidden::expose(|state| {
+        let block: &mut BlockTrace = state.traces.last_mut().expect("current block");
+        let trace = TxTrace {
+            block_id: block.block_id,
+            tx_id: tx.id,
+            ops: Vec::with_capacity(tx.ops.len()),
+        };
+        block.txs.push(trace);
+    });
 }
 
 pub fn on_op(op: &shared::Op) {
-    let state = STATE.get_or_init(|| Mutex::new(State::default()));
-    let mut guard = state.lock().unwrap();
-
-    let block = guard.traces.last_mut().expect("current block");
-    let tx = block.txs.last_mut().expect("current tx");
-    let trace = OpTrace { block_id: block.block_id, tx_id: tx.tx_id, val: op.0 };
-    tx.ops.push(trace);
+    hidden::expose(|state| {
+        let block = state.traces.last_mut().expect("current block");
+        let tx = block.txs.last_mut().expect("current tx");
+        let trace = OpTrace {
+            block_id: block.block_id,
+            tx_id: tx.tx_id,
+            val: op.0,
+        };
+        tx.ops.push(trace);
+    });
 }
